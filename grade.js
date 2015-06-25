c1a2086204ac783fa384a4ff25ff3ade6d10e6dc
Zach King <zachdk3@gmail.com>
2015-06-25 13:11:25 -0500

    fixed and commented
    i added comments to make things more clear and changed the video, i also
    fixed spelling and cleaned the js code a bit

--- grade.js
+++ grade.js
@@ -1,6 +1,6 @@
  function calculate() {
 
- 
+ // all the scores from the page  user input
   var s1=document.getElementById("score1").value;  
   var s2=document.getElementById("score2").value;
   var s3=document.getElementById("score3").value;
@@ -9,7 +9,7 @@
   var s6=document.getElementById("score6").value;
   var s7=document.getElementById("score7").value;
   var s8=document.getElementById("score8").value;
- 
+ //all thw grade weights from the page   user input
   var w1=document.getElementById("Weight1").value;
   var w2=document.getElementById("Weight2").value;
   var w3=document.getElementById("Weight3").value;
@@ -19,20 +19,20 @@
   var w7=document.getElementById("Weight7").value;
   var w8=document.getElementById("Weight8").value;
   
-  
+  //the ranges a-d if class is scaled differnt  user input
   var agoal=document.getElementById("ARange").value;
   var bgoal=document.getElementById("BRange").value;
   var cgoal=document.getElementById("CRange").value;
   var dgoal=document.getElementById("DRange").value;
    
-   
-   var w=100-w1-w2-w3-w4-w5-w6-w7-w8;
+   // w = total weight of class left
+  var w=100-w1-w2-w3-w4-w5-w6-w7-w8;
+  // total weight of completed class
   var donage=(100-w);
   
- 
-  var onetwo=s1*(w1/100)+s2*(w2/100);
-   
-   var threefour=s3*(w3/100)+s4*(w4/100);
+ //following calculate score x times  weight y and adds them in small groups for viewing purpose 
+  var onetwo=s1*(w1/100)+s2*(w2/100);   
+  var threefour=s3*(w3/100)+s4*(w4/100);
    
    var onefour= onetwo+threefour;
 
@@ -41,19 +41,21 @@
   
   var fiveeight= fivesix+eveight;
   
-var score=onefour+fiveeight;
-	var total=100*(donage/100);
-  var myaverage= (onefour+fiveeight)/total;
+  //completed total of score times weight
+ var score=onefour+fiveeight;
+ //
+  
+  //average for class so far
+  var myaverage= (onefour+fiveeight)/donage;
   
    document.getElementById("est").innerHTML = Math.round(myaverage*100);
- 
-  var dish= (dgoal-score)/((100-donage)/100);
-
-  var cish=(cgoal-score)/((100-donage)/100);
-  var bish=(bgoal-score)/((100-donage)/100);
+ // lists the grade required by the remaining percentage to get the goal grade
   var aish=(agoal-score)/((100-donage)/100);
-  
-   document.getElementById("apple").innerHTML = Math.round(aish);
+  var bish=(bgoal-score)/((100-donage)/100);
+  var cish=(cgoal-score)/((100-donage)/100);
+   var dish= (dgoal-score)/((100-donage)/100);
+   
+   document.getElementById("a").innerHTML = Math.round(aish);
 	document.getElementById("b").innerHTML = Math.round(bish);
 	document.getElementById("c").innerHTML = Math.round(cish);
 	document.getElementById("d").innerHTML = Math.round(dish);
